27e7511659663b804fff4d439d70f9163e405bf2
najia <najiaknizi199@gmail.com>
2024-09-02 09:46:16 +0100

    creation de interface de service pour l'entite service

---
+++ Bricoli_back/src/main/java/com/example/bricoli/service/ServiceService.java
@@ -0,0 +1,13 @@
+package com.example.bricoli.service;
+
+import com.example.bricoli.dto.ServiceDto;
+
+import java.util.List;
+
+public interface ServiceService {
+    ServiceDto create(ServiceDto serviceDto);
+    void delete(int id);
+    List<ServiceDto> getAll();
+    ServiceDto getServiceById(int id);
+    ServiceDto update(int id, ServiceDto serviceDto);
+}
---
+++ Bricoli_back/src/main/java/com/example/bricoli/service/ServiceServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.bricoli.service;
+
+public class ServiceServiceImpl {
+}
